73924143624e5929f98b21fee9a848e886632d7b
Tanzilalimam <gittanzilal@gmail.com>
2026-09-17 17:23:00 +0600

    u

--- src/components/pages/products/Cards.jsx
+++ src/components/pages/products/Cards.jsx
@@ -10,7 +10,7 @@ let [data, setData] = useState([]);
 
 useEffect(function(){
     async function viewData(){
-        let productApi = await axios.get('https://dummyjson.com/carts');
+        let productApi = await axios.get(`${import.meta.env.VITE_productApi}/carts`);
         setData(productApi.data.carts);
         console.log(productApi.data.carts);
     }
@@ -23,16 +23,16 @@ useEffect(function(){
                 <div className="flex justify-center items-center">
                     <div className="flex justify-center items-start flex-wrap gap-5">
                         {
-                            data.map(function(item){
-                                return item.products.map(function(product){
-                                return(
+                        data.map(function(item){
+                        return item.products.map(function(product){
+                        return(
                         <div key={product.id} className="pb-6 px-6 w-[25%] hover:bg-black/20 rounded-t-xl flex flex-col items-start gap-2.5 trans">
                         <Img src={product.thumbnail} alt='not found' className='w-full object-cover rounded-t-xl'></Img>
                         <h4 className="text-[18px] font-bold capitalize">{product.title}</h4>
                         <Para className="text-black" text={product.price}></Para>
                         <Btn className='bg-blue1/45 hover:bg-blue1 text-black1 hover:text-white trans' text='learn more'></Btn>
                         </div>
-                                    )
+                        )
                                 })
                             })
                         }
--- src/components/pages/recipes/Blocks.jsx
+++ src/components/pages/recipes/Blocks.jsx
@@ -10,9 +10,10 @@ let [data, setData] = useState([]);
 
 useEffect(function(){
     async function viewData(){
-        let recipeApi = await axios.get('https://dummyjson.com/recipes');
+        let recipeApi = await axios.get(`${import.meta.env.VITE_recipeApi}/recipes`);
         setData(recipeApi.data.recipes);
         console.log(recipeApi.data.recipes);
+        console.log(`${import.meta.env.VITE_name}`);
     }
     viewData();
 }, []);
@@ -24,11 +25,20 @@ useEffect(function(){
                 {
                     data.map(function(recipe){
                     return(
-                    <div key={recipe.id} className="group w-[25%] hover:bg-black/10 flex flex-col items-start gap-2.5 rounded-t-xl trans">
+                    <div key={recipe.id} className="group w-[25%] h-150 overflow-y-scroll scrollbar-none hover:bg-black/10 flex flex-col items-start gap-2.5 rounded-t-xl trans">
                     <Img src={recipe.image} alt='no pic' className='w-full object-cover rounded-t-xl'></Img>
                     <div className="group-hover:pb-6 group-hover:px-6 flex flex-col items-start gap-2.5 trans">
                     <h4 className="text-[18px] font-bold capitalize">{recipe.name}</h4>
-                    <Para className="text-black1 text-[16px] font-semibold" text={recipe.ingredients}></Para>
+                    <ul className="px-4 flex flex-col gap-2">
+                    {
+                    recipe.ingredients.map(function(i){
+                        return(
+                            <li className="list-disc font-semibold">{i}</li>
+                        )
+                    })
+                    }
+                    </ul>
+                    <Para className="text-black1 text-[16px] font-semibold" text={recipe.instructions}></Para>
                     <Btn className='bg-blue1/45 hover:bg-blue1 text-black1 hover:text-white trans' text='read more'></Btn>
                     </div>
                     </div>
